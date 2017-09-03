929b61acd190d020010dc098b540421f8123537d
Arthur BABEY <git@ababey.com>
2017-09-03 17:27:29 +0200

    Init & First scene

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour 
+{
+    public void LoadLevel(string name) 
+    {
+        Debug.Log("Hola bonitta");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Ne me quittes pas ...");
+    }
+	
+}
